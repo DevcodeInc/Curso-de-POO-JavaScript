f2ea99603956962fcaaa693d7dda0232307dff19
Enrique Munguía <enrique7mc@gmail.com>
2017-04-16 13:21:13 -0700

    añade más métodos a DataStore

--- datastore.js
+++ datastore.js
@@ -4,4 +4,16 @@ function DataStore() {
 
 DataStore.prototype.add = function (key, val) {
   this.data[key] = val;
-}
+};
+
+DataStore.prototype.get = function (key) {
+  return this.data[key];
+};
+
+DataStore.prototype.getAll = function () {
+  return this.data;
+};
+
+DataStore.prototype.remove = function (key) {
+  delete this.data[key];
+};
--- main.js
+++ main.js
@@ -3,5 +3,7 @@ var dataStore = new DataStore();
 dataStore.add('valor1', 'esta es una cadena');
 dataStore.add('valor2', 20);
 console.log(dataStore);
+console.log(dataStore.getAll());
+console.log(dataStore.get('valor2'));
 
 console.log(DataStore.prototype === dataStore.__proto__);
