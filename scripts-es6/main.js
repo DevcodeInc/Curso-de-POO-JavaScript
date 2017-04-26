95a4da3b05b071a83ec731fa10d2fd686350fa07
Enrique Munguía <enrique7mc@gmail.com>
2017-04-26 06:50:56 -0700

    añade front end para añadir órdenes

--- scripts-es6/main.js
+++ scripts-es6/main.js
@@ -3,13 +3,30 @@ import Store from './store';
 import { PizzaOrder } from './order';
 
 const store = new Store('ncc-1701', new DataStore());
-const order = PizzaOrder({
-  email: 'enrique@devcode.com',
-  size: 'large',
-  speciality: 'meats'
-});
-store.createOrder(order);
-store.printOrders();
-order.displayOrder();
-store.deliverOrder('enrique@devcode.com');
-store.printOrders();
+const form = document.querySelector('#pizza-form');
+form.addEventListener('submit', onSubmit);
+const checklist = document.querySelector('#checklist');
+
+// Event Handlers
+function onSubmit(e) {
+  e.preventDefault();
+  const email = form.querySelector('#emailInput').value;
+  const size = form.querySelector('input[name="size"]:checked').value;
+  const select = form.querySelector('#speciality');
+  const speciality = select.options[select.selectedIndex].value;
+  const newOrder = new PizzaOrder({
+    email,
+    size,
+    speciality
+  });
+  store.createOrder(newOrder);
+  addToPending(newOrder);
+}
+
+// Funciones
+function addToPending(order) {
+  const orderDiv = document.createElement('div');
+  const newContent = document.createTextNode(order.toString());
+  orderDiv.appendChild(newContent);
+  checklist.appendChild(orderDiv);
+}
--- scripts-es6/order.js
+++ scripts-es6/order.js
@@ -1,18 +1,46 @@
+/* // Herencia con clases
+class BaseOrder {
+  constructor(email) {
+    this.email = email;
+  }
+}
+
+class PizzaOrder extends BaseOrder {
+  constructor(email, size, speciality) {
+    super(email);
+    this.size = size;
+    this.speciality = speciality;
+  }
+}
+
+export {
+  BaseOrder,
+  PizzaOrder
+};
+*/
+
+// Composición
 const email = { email: '' };
 const size = { size: 'medium' };
 const speciality = { speciality: 'cheese' };
 const displayOrder = {
   displayOrder: function () {
-    console.log(`Order for: ${this.email}`);
+    console.log('Order for: ' + this.email);
   }
-}
+};
 
 const BaseOrder = (params) => {
   return Object.assign({}, email, displayOrder, params);
 };
 
+const toString = {
+  toString: function () {
+    return `${this.email} - ${this.size} - ${this.speciality}`;
+  }
+};
+
 const PizzaOrder = (params) => {
-  return Object.assign({}, email, size, displayOrder, speciality, params);
+  return Object.assign({}, email, size, speciality, displayOrder, toString, params);
 };
 
 export {
